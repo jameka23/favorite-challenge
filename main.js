a315092ba25f562217f72feade85705a3356d605
Jameka Echols <jameka.echols@gmail.com>
2019-03-06 18:17:00 -0600

    added stuff

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const locations = document.getElementById("location")
 const wishlist = document.querySelector(".wishlist")
 
 document.getElementById("saveButton").addEventListener("click", function(event){
-    wishlist.innerHTML = `I can purchase ${things.value} at ${locations.value}.`
-    things.value = "" // clears out the things input field 
-    locations.value = "" // clears out the locations' input field 
-    things.focus() // focuses on the first input tag 
+    if(things.value === "" || locations.value === ""){
+        alert("Please enter all information!")
+    }else{
+        wishlist.innerHTML = `I can purchase ${things.value} at ${locations.value}.`
+        things.value = "" // clears out the things input field 
+        locations.value = "" // clears out the locations' input field 
+        things.focus() // focuses on the first input tag 
+    }
 })
